5b4812b652ae09f53814d6e874a83cae3123a619
Roberto <robigarci@gmail.com>
2014-01-01 23:31:50 -0500

    varios 2.0

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,15 @@
 function init() {
+        //Variables
+        var score = 0;
+        var jugar = false;
+        
+        
         //initialize the stage
         canvas = document.getElementById("canvas");
         stage = new createjs.Stage(canvas);
         
+        
+       
         //adding the background image
         var text = new createjs.Text ("Dos + dos", "36px Arial", "#666");
        
@@ -15,4 +22,20 @@ function init() {
         stage.addChild(text);
         //updating the stage
         stage.update();
+        
+        
+        var smarty = new Button();
+        smarty.x = 205;
+        smarty.y = 45;
+
+        //include each state
+        smarty.up( new Bitmap( 'assets/_up.png') );
+        smarty.over( new Bitmap( 'assets/_over.png') );
+        smarty.down( new Bitmap( 'assets/_down.png') );
+        
+        //handle each mouse action
+        smarty.press = function(event){ log( 'smarty press' ); };
+        smarty.rollOver = function(event){ log( 'smarty rollOver' ); };
+        smarty.rollOut = function(event){ log( 'smarty rollOut' ); };
+        smarty.release = function(event){ log( 'smarty release' ); };
 }
